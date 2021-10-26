35b170cbd7783e122dae03b77a03974320a353e4
Malte Hoch <mhoch94@gmail.com>
2021-10-26 20:41:48 +0200

    Remove Todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,14 +2,26 @@ import React, {useState} from 'react';
  
 export default function TodoList(props) {
     const[todos, setTodos] = useState([
-        { task: "Walk the fish" },
-        { task: "Groom Chickens" } 
+        "Walk the cat",
+        "Make the garden",
     ]);
+
+    const getTodos = () => {
+        let output = [];
+        for(let x = 0; x < todos.length; x++) {
+            output = [...output, (<p key={x}>{todos[x]}<button className="btn-delete" onClick={() => deleteItem(x)}>x</button> </p>)];
+        }
+        return output;
+    }
+
+    const deleteItem = (id) => {
+        setTodos(todos.splice(id - 1, 1));
+    }
 
 
   return (
-      <div>
-          {todos}
+      <div className="todo-list">
+          {getTodos()}
       </div>
   )
 }
